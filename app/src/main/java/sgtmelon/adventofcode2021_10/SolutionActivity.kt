58df4a5052a69455812a070d00b99e1ba598e740
SerjantArbuz <lex.kuznet@gmail.com>
2022-10-29 16:00:30 +0300

    First commit

---
+++ app/src/main/java/sgtmelon/adventofcode2021_10/SolutionActivity.kt
@@ -0,0 +1,15 @@
+package sgtmelon.adventofcode2021_10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+
+class SolutionActivity : AppCompatActivity() {
+
+    private val viewModel: SolutionViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/sgtmelon/adventofcode2021_10/SolutionViewModel.kt
@@ -0,0 +1,6 @@
+package sgtmelon.adventofcode2021_10
+
+import androidx.lifecycle.ViewModel
+
+class SolutionViewModel : ViewModel() {
+}

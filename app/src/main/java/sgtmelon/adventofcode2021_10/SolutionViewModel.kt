652ff556c15b6d484614bf22228e406fa35bcac5
SerjantArbuz <lex.kuznet@gmail.com>
2022-10-29 16:50:35 +0300

    Add check for line-full condition

--- app/src/main/java/sgtmelon/adventofcode2021_10/SolutionViewModel.kt
+++ app/src/main/java/sgtmelon/adventofcode2021_10/SolutionViewModel.kt
@@ -5,19 +5,22 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import sgtmelon.adventofcode2021_10.useCase.IsLineFullUseCase
 import sgtmelon.adventofcode2021_10.useCase.SplitTextUseCase
 
 class SolutionViewModel(
-    private val splitText: SplitTextUseCase
+    private val splitText: SplitTextUseCase,
+    private val isLineFull: IsLineFullUseCase
 ) : ViewModel() {
 
     fun getResult(input: String): Flow<Int> = flow {
-        val inputList = splitText(input)
+        val lineList = splitText(input)
 
+        for (line in lineList) {
+            if (!isLineFull(line)) continue
+        }
 
-
-
-        emit(inputList.size)
+        emit(lineList.size)
     }.flowOn(Dispatchers.IO)
 
 }
--- app/src/main/java/sgtmelon/adventofcode2021_10/di/SolutionInjectionModule.kt
+++ app/src/main/java/sgtmelon/adventofcode2021_10/di/SolutionInjectionModule.kt
@@ -3,6 +3,7 @@ package sgtmelon.adventofcode2021_10.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import sgtmelon.adventofcode2021_10.SolutionViewModel
+import sgtmelon.adventofcode2021_10.useCase.IsLineFullUseCase
 import sgtmelon.adventofcode2021_10.useCase.SplitTextUseCase
 
 object SolutionInjectionModule {
@@ -10,11 +11,15 @@ object SolutionInjectionModule {
     val module = module {
 
         viewModel {
-            SolutionViewModel(get())
+            SolutionViewModel(get(), get())
         }
 
         factory {
             SplitTextUseCase()
+        }
+
+        factory {
+            IsLineFullUseCase()
         }
     }
 }

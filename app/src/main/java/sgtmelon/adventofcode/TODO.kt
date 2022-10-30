2a5c5a6c574bb027691851fa0d35b8c6d29128dc
SerjantArbuz <lex.kuznet@gmail.com>
2022-10-30 16:32:54 +0300

    Add variable for fast access to some class

--- app/src/main/java/sgtmelon/adventofcode/TODO.kt
+++ app/src/main/java/sgtmelon/adventofcode/TODO.kt
@@ -1,4 +1,2 @@
 package sgtmelon.adventofcode
 
-// TODO add variable for fast starting of app while debuging (like deep link to another screen)
-// TODO add parent solution class (with toolbar to back)
--- app/src/main/java/sgtmelon/adventofcode/menu/MainMenuActivity.kt
+++ app/src/main/java/sgtmelon/adventofcode/menu/MainMenuActivity.kt
@@ -1,10 +1,13 @@
 package sgtmelon.adventofcode.menu
 
+import android.content.Intent
+import android.os.Bundle
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import sgtmelon.adventofcode.R
 import sgtmelon.adventofcode.menu.adapter.YearAdapter
 import sgtmelon.adventofcode.menu.model.Year
 import sgtmelon.adventofcode.parent.ParentMenuActivity
+import sgtmelon.adventofcode.year21.day10.SubmarineActivity
 
 class MainMenuActivity : ParentMenuActivity(),
     YearAdapter.Callback {
@@ -14,6 +17,17 @@ class MainMenuActivity : ParentMenuActivity(),
     private val viewModel: MainMenuViewModel by viewModel<MainMenuViewModelImpl>()
 
     override val adapter = YearAdapter(callback = this)
+
+    /** Change this variable for open specific screen (class) directly (skipping the menu). */
+    private val fastAccessClass: Class<*>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (fastAccessClass != null) {
+            startActivity(Intent(this, fastAccessClass))
+        }
+    }
 
     override fun setupObservers() {
         super.setupObservers()
